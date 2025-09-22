2763ee82394dccce1518042f0c5010dbbdfc928c
nahan-shawkkathali <nahanstory@gmail.com>
2025-09-22 16:10:12 +0530

    Add files via upload

---
+++ merge_dict (2).py
@@ -0,0 +1,7 @@
+dict1 = {"name": "Rose", "age": 7, "gender": "female"}
+dict2 ={"place": "italy", "class": 2} 
+merge=dict1.copy()
+merge.update(dict2)
+print(merge)
+
+
---
+++ sum_dict.py
@@ -0,0 +1,3 @@
+dict={"1":10, "2":20}
+total=sum(dict.values())
+print("sum=",total)
